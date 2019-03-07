f612efc3e52776c626a002ab31194cda94b752af
Matteo Tafani Alunno <tafax@users.noreply.github.com>
2019-03-07 22:52:02 +0100

    fix(build): Build script broken (#14)
    close #13

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]] || [[ $TRAVIS_BRANCH != "devel" ]]
+if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]] || [[ $TRAVIS_BRANCH != "master" ]]
     then
         echo "This script only works for MASTER and for non-CRON tasks..."
         exit 0
 fi
 
+git remote set-url origin https://tafax:$GITHUB_TOKEN@github.com/tafax/js-bus.git
+git checkout master
+git pull --tags origin master
+
 ./node_modules/.bin/npm-cli-login
-./node_modules/.bin/release-it --non-interactive
+./node_modules/.bin/release-it --non-interactive --verbose 0.0.5
