df76265132be25540bf6208c34b83212c955664d
Matteo Tafani Alunno <tafax@users.noreply.github.com>
2019-03-08 17:22:07 +0100

    feat(bus): Allow to execute a message handler without a subscription (#21)
    close #15

--- scripts/publish-debug.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-./node_modules/.bin/npm-cli-login
-./node_modules/.bin/release-it --non-interactive --dry-run --no-git.requireCleanWorkingDir --verbose 0.0.5
--- tests/integration/query-bus/query-bus.integration.tests.ts
+++ tests/integration/query-bus/query-bus.integration.tests.ts
@@ -1,17 +1,17 @@
 
 import { suite, test, Mock, IMock, Times } from '@js-bus/test';
+import { of } from 'rxjs';
 import { MessageBusAllowMiddleware } from '../../../src/lib/bus/message-bus-allow-middleware';
 import { ServiceLocatorAwareCallableResolver } from '../../../src/lib/callable-resolver/service-locator-aware.callable-resolver';
 import { MessageHandlingCollection } from '../../../src/lib/collection/message-handling.collection';
 import { FunctionConstructorMessageTypeExtractor } from '../../../src/lib/extractor/function-constructor.message-type-extractor';
 import { ObservableDelegatesMessageHandlerMiddleware } from '../../../src/lib/handler/observable-delegates-message-handler.middleware';
 import { ClassMapHandlerResolver } from '../../../src/lib/resolver/class-map.handler-resolver';
-
-import { GoodQueryHandlerForTest } from './utility/good-query-handler-for-test';
-import { GoodQueryForTest } from './utility/good-query-for-test';
-import { EvilQueryForTest } from './utility/evil-query-for-test';
-import { EvilQueryHandlerForTest } from './utility/evil-query-handler-for-test';
-import { CustomError } from './utility/custom.error';
+import { CustomError } from '../../fixtures/custom.error';
+import { EvilQueryForTest } from '../../fixtures/evil-query-for-test';
+import { EvilQueryHandlerForTest } from '../../fixtures/evil-query-handler-for-test';
+import { GoodQueryForTest } from '../../fixtures/good-query-for-test';
+import { GoodQueryHandlerForTest } from '../../fixtures/good-query-handler-for-test';
 
 @suite class QueryBusIntegrationTests {
 
@@ -40,35 +40,72 @@ import { CustomError } from './utility/custom.error';
     ]);
   }
 
-  @test 'should execute the correct query handler and fulfill with result value'() {
+  @test 'should execute the correct query handler and return the result value'() {
 
-    const command = new GoodQueryForTest();
+    const query = new GoodQueryForTest();
 
     this.serviceLocatorMock
       .setup(x => x(GoodQueryHandlerForTest))
       .returns(() => new GoodQueryHandlerForTest())
       .verifiable(Times.once());
 
-    return this.queryBus.handle(command)
+    return this.queryBus.handle(query)
       .subscribe(
-        (result: any) => { result.should.be.eql('result-value'); },
-        () => { throw new Error('should-not-be-called'); }
+        (result: string) => {
+          result.should.be.eql('result-value');
+
+          this.serviceLocatorMock.verifyAll();
+        },
       );
+  }
+
+  @test 'should execute the correct query handler and return the result value without subscribing'() {
+
+    const query = new GoodQueryForTest();
+
+    const queryHandlerMock = Mock.ofType(GoodQueryHandlerForTest);
+
+    queryHandlerMock
+      .setup(x => x.handle(query))
+      .returns(() => of('result-value'))
+      .verifiable(Times.once());
+
+    this.serviceLocatorMock
+      .setup(x => x(GoodQueryHandlerForTest))
+      .returns(() => queryHandlerMock.object)
+      .verifiable(Times.once());
+
+    const execution$ = this.queryBus.handle(query);
+
+    this.serviceLocatorMock.verifyAll();
+    queryHandlerMock.verifyAll();
+
+    return execution$
+      .subscribe((result: string) => {
+        result.should.be.eql('result-value');
+
+        this.serviceLocatorMock.verifyAll();
+        queryHandlerMock.verifyAll();
+      });
   }
 
   @test 'should execute the correct command handler and reject'() {
 
-    const command = new EvilQueryForTest();
+    const query = new EvilQueryForTest();
 
     this.serviceLocatorMock
       .setup(x => x(EvilQueryHandlerForTest))
       .returns(() => new EvilQueryHandlerForTest())
       .verifiable(Times.once());
 
-    return this.queryBus.handle(command)
+    return this.queryBus.handle(query)
       .subscribe(
         () => { throw new Error('should-not-be-called'); },
-        (error: Error) => { error.should.be.instanceof(CustomError); }
+        (error: Error) => {
+          error.should.be.instanceof(CustomError);
+
+          this.serviceLocatorMock.verifyAll();
+        }
       );
   }
 }
